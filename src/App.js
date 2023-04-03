9010e58b7e74c9ece0b43860addecbea6b6138cf
caelidh <ceilidhliddell@gmail.com>
2023-04-02 18:31:58 -0700

    added content

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       
+      <footer>This project was coded by Caelidh Liddell and is open-sourced on <a href="https://github.com/caelidh/weather-react">Github</a></footer>
     </div>
   );
 }
