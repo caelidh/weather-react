d16101d608a3a2c7c39d5951f76134c2eb495db7
caelidh <ceilidhliddell@gmail.com>
2023-04-03 16:13:27 -0700

    added rel to github link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <div className="container">
       <Weather defaultCity="New York" />
-      <footer>This project was coded by Caelidh Liddell and is open-sourced on <a href="https://github.com/caelidh/weather-react" target="_blank">Github</a></footer>
+      <footer>This project was coded by Caelidh Liddell and is open-sourced on <a href="https://github.com/caelidh/weather-react" target="_blank" rel="noreferrer">Github</a></footer>
     </div>
     </div>
   );
