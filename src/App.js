ce88070d1a0b48cbdabecaa039a3f116241c7e1e
caelidh <ceilidhliddell@gmail.com>
2023-04-02 21:06:06 -0700

    created weatherinfo.js and updated content

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather />
+      <Weather defaultCity="New York" />
       <footer>This project was coded by Caelidh Liddell and is open-sourced on <a href="https://github.com/caelidh/weather-react" target="_blank">Github</a></footer>
     </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,46 +1,62 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
-import FormattedDate from "./FormattedDate";
 import "./Weather.css";
+import WeatherInfo from "./WeatherInfo";
 
 
-export default function Weather() {
+export default function Weather(props) {
+    const [city, setCity] = useState(props.defaultCity);
+    const [weatherData, setWeatherData] = useState({ ready: false });
+
+    function handleResponse(response) {
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            city: response.data.name,
+            wind:response.data.wind.speed,
+            description: response.data.weather[0].description,
+            humidity: response.data.main.humidity,
+            iconUrl: "data:image/png;basedata:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAUhJREFUeNrt230NgzAQh2GkIAUJyJgMJCABCZNQKcxBHdyOpFkyEkYGV9oL75Lff2Rwz8pXe2tEpLlzGgAAAAAAAAAAYHsDg4+8Hp1m0ATNrJGNzGmbZdvOZN+lAFLRkyb+KHgvMX1H5wYgFR5OFL2VcATiMgA9uFbzzFD4Oss+2qoA9ID6k0P9yKnRVwGgBzJeWPg6Y1GAdIGSwpmKAFRS/C5CFoDCw/6v08EcIF3wpNL0WQHSrS5WDBDXt0hrgGfFxX+eE7IApCc8cZIuB0BwBBBMAZz9+l+jwApgcggwWQJEhwDRBMDp8P+cBhYAg2OAwQIgOAYIFgCzY4DZAkBcB4C7AywvcIwAAADgNsiDEI/CvAzxOsyECFNiTIoyLc7CCEtjLI6yPE6DBC0yNEnRJkejJK2yNEvTLs8fJgAAAAAAAADg1nkDlR7XfJiH1ggAAAAASUVORK5CYII=64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAUhJREFUeNrt230NgzAQh2GkIAUJyJgMJCABCZNQKcxBHdyOpFkyEkYGV9oL75Lff2Rwz8pXe2tEpLlzGgAAAAAAAAAAYHsDg4+8Hp1m0ATNrJGNzGmbZdvOZN+lAFLRkyb+KHgvMX1H5wYgFR5OFL2VcATiMgA9uFbzzFD4Oss+2qoA9ID6k0P9yKnRVwGgBzJeWPg6Y1GAdIGSwpmKAFRS/C5CFoDCw/6v08EcIF3wpNL0WQHSrS5WDBDXt0hrgGfFxX+eE7IApCc8cZIuB0BwBBBMAZz9+l+jwApgcggwWQJEhwDRBMDp8P+cBhYAg2OAwQIgOAYIFgCzY4DZAkBcB4C7AywvcIwAAADgNsiDEI/CvAzxOsyECFNiTIoyLc7CCEtjLI6yPE6DBC0yNEnRJkejJK2yNEvTLs8fJgAAAAAAAADg1nkDlR7XfJiH1ggAAAAASUVORK5CYII=",
+            date: new Date(response.data.dt * 1000),
+
+        });
+    }
+    
+function handleSubmit(event) {
+    event.preventDefault();
+    alert(city);
+    
+}
+
+function handleCityChange(event) {
+    setCity(event.target.value);
+}   
+
+function search() {
+        const apiKey = "717511f5e1c0dbfc617f361ab073e2e9";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+    }
+
+    if (weatherData.ready) {
 return (
     <div className="Weather">
-        <form>
+        <form onSumbit={handleSubmit}> 
             <div className="row">  
             <div className="col-9">
-            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
+            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" onChange={handleCityChange} />
             </div>
             <div className="col-3">
             <input type="submit" value="Search" className="btn btn-primary w-100" />
        </div>
        </div> 
        </form>
-<h1>New York</h1>
-<ul>
-    <li>Sunday 22:00</li>
-    <li>Clear</li>
-    </ul>
-    <div className="row mt-3">
-        <div className="col-6">
-<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAUhJREFUeNrt230NgzAQh2GkIAUJyJgMJCABCZNQKcxBHdyOpFkyEkYGV9oL75Lff2Rwz8pXe2tEpLlzGgAAAAAAAAAAYHsDg4+8Hp1m0ATNrJGNzGmbZdvOZN+lAFLRkyb+KHgvMX1H5wYgFR5OFL2VcATiMgA9uFbzzFD4Oss+2qoA9ID6k0P9yKnRVwGgBzJeWPg6Y1GAdIGSwpmKAFRS/C5CFoDCw/6v08EcIF3wpNL0WQHSrS5WDBDXt0hrgGfFxX+eE7IApCc8cZIuB0BwBBBMAZz9+l+jwApgcggwWQJEhwDRBMDp8P+cBhYAg2OAwQIgOAYIFgCzY4DZAkBcB4C7AywvcIwAAADgNsiDEI/CvAzxOsyECFNiTIoyLc7CCEtjLI6yPE6DBC0yNEnRJkejJK2yNEvTLs8fJgAAAAAAAADg1nkDlR7XfJiH1ggAAAAASUVORK5CYII=" alt="Clear" className="float-left" />
-      <span className="temperature">46</span><span className="unit">°F</span> 
-      </div>
-      <div className="col-6">
-        <ul>
-            <li>Precipitation: 0% </li>
-            <li>Humidity: 25%</li>
-            <li>Wind: 5 mph</li>
-        </ul>
-        </div>
+       <WeatherInfo data={weatherData} />
     </div>
-    
-
-
-    </div>
-)
-
-  ;
+);
+} else {
+    search();
+return "Loading...";
+}
 }
